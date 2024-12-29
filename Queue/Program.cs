f6f754ba63f5b0d3978afc482817833285d1f11a
DiamondAngel1 <tararukolga78@gmail.com>
2024-12-29 19:45:07 +0200

    first commit

---
+++ Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Queue{
+    internal class Program{
+        static void Main(string[] args){
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            PriorityQueue<string> queue = new PriorityQueue<string>();
+            queue.AddElement("Середній приорітет", 2);
+            queue.AddElement("Низький приорітет", 3);
+            queue.AddElement("Високий прорітет", 1);
+            Console.WriteLine("Кількість елементів в черзі: " + queue.Count());
+            Console.WriteLine("Найвища приоритетність: " + queue.Bigger());
+            while (!queue.Empty){
+                Console.WriteLine(queue.DelElement());
+            }
+        }
+    }
+}
---
+++ Queue/QueuesPrioritet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue{
+    public class PriorityQueue<T>{
+        private List<(T Item, int Priority)> queue = new List<(T, int)>();
+        public void AddElement(T item, int priority){
+            queue.Add((item, priority));
+            SortQueue();
+        }
+        private void SortQueue(){
+            for (int i = 0; i < queue.Count - 1; i++){
+                for (int j = 0; j < queue.Count - i - 1; j++){
+                    if (queue[j].Priority > queue[j + 1].Priority){
+                        var temp = queue[j];
+                        queue[j] = queue[j + 1];
+                        queue[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public T DelElement(){
+            if (Empty){
+                throw new InvalidOperationException("Черга пуста");
+            }
+            T item = queue[0].Item;
+            queue.RemoveAt(0);
+            return item;
+        }
+        public T Bigger(){
+            if (Empty){
+                throw new InvalidOperationException("Черга пуста");
+            }
+            return queue[0].Item;
+        }
+        public bool Empty{
+            get{
+                return queue.Count == 0;
+            }
+        }
+        public int Count(){
+            return queue.Count;
+        }
+    }
+}
